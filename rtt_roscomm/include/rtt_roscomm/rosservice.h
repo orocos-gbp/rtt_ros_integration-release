71d9896bff643d98ad35bb5118dceeaee21aa6ff
Johannes Meyer <johannes@intermodalics.eu>
2017-11-16 10:05:29 +0100

    Revert "Trimmed the branch to only the typekits/rtt_std_msgs sub directory"
    This reverts commit 0153445194db0d1c88d21a1df2d73ffce67bb0ab.

---
+++ rtt_roscomm/include/rtt_roscomm/rosservice.h
@@ -0,0 +1,30 @@
+#ifndef __RTT_ROSSERVICE_ROSSERVICE_H
+#define __RTT_ROSSERVICE_ROSSERVICE_H
+
+#include <rtt/RTT.hpp>
+#include <rtt/Property.hpp>
+
+namespace rtt_rosservice {
+
+  class ROSService : public RTT::ServiceRequester
+  {
+  public:
+    ROSService(RTT::TaskContext *owner) :
+      RTT::ServiceRequester("rosservice",owner),
+      connect("connect"),
+      disconnect("disconnect"),
+      disconnectAll("disconnectAll")
+    {
+      this->addOperationCaller(connect);
+      this->addOperationCaller(disconnect);
+      this->addOperationCaller(disconnectAll);
+    }
+
+    RTT::OperationCaller<bool(const std::string &, const std::string &, const std::string &)> connect;
+    RTT::OperationCaller<bool(const std::string &)> disconnect;
+    RTT::OperationCaller<void()> disconnectAll;
+  };
+}
+
+#endif // ifndef __RTT_ROSSERVICE_ROSSERVICE_H
+
---
+++ rtt_rosnode/CHANGELOG.rst
@@ -0,0 +1,18 @@
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+Changelog for package rtt_rosnode
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+2.8.5 (2017-03-28)
+------------------
+
+2.8.4 (2016-11-26)
+------------------
+
+2.8.3 (2016-07-20)
+------------------
+* rtt_rosnode: fixed corruption of RTT's argv vector when loading the rosnode plugin (fix `#54 <https://github.com/orocos/rtt_ros_integration/issues/54>`_)
+* Contributors: Johannes Meyer
+
+2.8.2 (2015-06-12)
+------------------
+* see `rtt_ros_integratoin/CHANGELOG.rst <../rtt_ros_integration/CHANGELOG.rst>`_
